596f3f94b843b2eb1a749b33a63cf8ccb668f6b4
Joao Ribeiro <jonnybgod@gmail.com>
2016-06-12 19:14:03 +0100

    Separate generated files

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 
-exports.services = require('./lib/services');
+exports.angularjs = require('./lib/angularjs');
+
+exports.angular = require('./lib/angular');
+exports.ngrx = require('./lib/ngrx');
+
+exports.nativescript2 = require('./lib/nativescript2');
---
+++ test/fixtures/app.js
@@ -0,0 +1,6 @@
+var loopback = require('loopback');
+var app = loopback();
+
+app.set('restApiRoot', '/rest-api-root');
+
+module.exports = app;
