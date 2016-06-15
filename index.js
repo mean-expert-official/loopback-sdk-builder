9666720a95e17ce2dac8407cad1e5329d4c6e23c
Jonathan Casarrubias <c_jcasarrubias@groupon.com>
2016-06-15 12:34:45 -0500

    Created common directory

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-exports.angularjs = require('./lib/angularjs');
-
-exports.angular = require('./lib/angular');
-exports.ngrx = require('./lib/ngrx');
-
+exports.angular2      = require('./lib/angular2');
+exports.ngrx          = require('./lib/ngrx');
 exports.nativescript2 = require('./lib/nativescript2');
