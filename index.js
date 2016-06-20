75880316233fc4f5f25f4decf5aca5de66a7491b
Jonathan Casarrubias <c_jcasarrubias@groupon.com>
2016-06-20 16:45:12 -0500

    Refactor and Tweaks beta 9

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-exports.angular2      = require('./lib/angular2');
-exports.ngrx          = require('./lib/ngrx');
-exports.nativescript2 = require('./lib/nativescript2');
+exports.angular2 = require('./lib/angular2');
+exports.ngrx = require('./lib/ngrx');
+//exports.react = require('./lib/react'); TODO
