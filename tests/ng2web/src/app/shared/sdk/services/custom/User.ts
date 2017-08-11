6a30b3016d2f6f4804bd7b79d4b64dd178507ac7
Jonathan Casarrubias <jcasarrubias@mean.expert>
2017-08-11 12:48:14 -0500

    Release 2.1.0-rc.13.4
    - Replaces Release 2.1.0-rc.13.3

--- tests/ng2web/src/app/shared/sdk/services/custom/User.ts
+++ tests/ng2web/src/app/shared/sdk/services/custom/User.ts
@@ -480,8 +480,10 @@ export class UserApi extends BaseLoopBackApi {
     "/Users/change-password";
     let _routeParams: any = {};
     let _postBody: any = {
-      oldPassword: oldPassword,
-      newPassword: newPassword
+      data: {
+        oldPassword: oldPassword,
+        newPassword: newPassword
+      }
     };
     let _urlParams: any = {};
     if (typeof oldPassword !== 'undefined' && oldPassword !== null) _urlParams.oldPassword = oldPassword;
@@ -509,7 +511,9 @@ export class UserApi extends BaseLoopBackApi {
     "/Users/reset-password";
     let _routeParams: any = {};
     let _postBody: any = {
-      newPassword: newPassword
+      data: {
+        newPassword: newPassword
+      }
     };
     let _urlParams: any = {};
     if (typeof newPassword !== 'undefined' && newPassword !== null) _urlParams.newPassword = newPassword;
