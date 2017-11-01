48f5515301cfc68203493947a77fc4fec61d52d6
Joao Ribeiro <jonnybgod@gmail.com>
2017-11-01 20:23:34 +0000

    compatibility with Angular 5

--- tests/ng2web/src/app/shared/sdk/orm/index.ts
+++ tests/ng2web/src/app/shared/sdk/orm/index.ts
@@ -2,40 +2,27 @@
 import {
   NgModule,
   ModuleWithProviders,
-  Inject,
   Optional,
-  OpaqueToken,
   SkipSelf
 } from '@angular/core';
 import { Orm } from './orm';
 
 export * from './orm';
 
-export const ORM_FORROOT_GUARD = new OpaqueToken('ORM_FORROOT_GUARD');
-export function provideForRootGuard(orm: Orm): any {
-  if (orm) {
-    throw new Error(
-      `OrmModule.forRoot() called twice. Lazy loaded modules should use OrmModule.forChild() instead.`);
-  }
-
-  return 'guarded';
-}
-
 @NgModule({})
 export class OrmModule {
+  constructor(@Optional() @SkipSelf() parentModule: OrmModule) {
+    if (parentModule) {
+      throw new Error(
+        'OrmModule.forRoot() called twice. Lazy loaded modules should use OrmModule instead.',
+      );
+    }
+  }
+
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: OrmModule,
-      providers: [
-        {
-          provide: ORM_FORROOT_GUARD,
-          useFactory: provideForRootGuard,
-          deps: [[Orm, new Optional(), new SkipSelf()]]
-        },
-        Orm
-      ]
+      providers: [ Orm ]
     };
   }
-
-  constructor(@Optional() @Inject(ORM_FORROOT_GUARD) guard: any) {}
 }
