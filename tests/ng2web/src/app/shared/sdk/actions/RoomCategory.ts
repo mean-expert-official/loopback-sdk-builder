2281535933cd220252ccc92e3c616e97179be047
Joao Ribeiro <jonnybgod@gmail.com>
2017-12-03 23:25:39 +0000

    remove generated code

--- tests/ng2web/src/app/shared/sdk/actions/RoomCategory.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-import { Action } from '@ngrx/store';
-import { type } from '../util';
-import { BaseLoopbackActionTypesFactory, BaseLoopbackActionsFactory } from './base';
-import { LoopBackFilter, SDKToken, RoomCategory } from '../models';
-
-export const RoomCategoryActionTypes = Object.assign(BaseLoopbackActionTypesFactory('RoomCategory'));
-
-export const RoomCategoryActions = Object.assign(BaseLoopbackActionsFactory<RoomCategory>(RoomCategoryActionTypes));
--- tests/ng2web/src/app/shared/sdk/resolvers/index.ts
+++
@@ -1,2 +0,0 @@
-/* tslint:disable */
-export * from './auth-account';
