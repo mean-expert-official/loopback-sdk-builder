847c2f0b13e9566551f76a98ae1dd05999e95770
Vitali Nedzvedz <vitali.nedzvedz@leverx.com>
2018-02-17 15:33:02 +0300

    Revert back server path at LoopBackConfig.

--- tests/ng2web/src/app/shared/sdk/lb.config.ts
+++ tests/ng2web/src/app/shared/sdk/lb.config.ts
@@ -20,7 +20,7 @@
 * }
 **/
 export class LoopBackConfig {
-  private static path: string = '//localhost:3000';
+  private static path: string = '//0.0.0.0:3000';
   private static version: string | number = 'api';
   private static authPrefix: string = '';
   private static debug: boolean = true;
